99fda374f269f70011760f6c1182a4aa21208344
DaniaFHG <daniafernanda.15@gmail.com>
2021-09-08 00:19:10 -0600

    angular demo code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppComponent } from './app.component';
+import { EstudianteComponent } from './estudiante/estudiante.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudianteComponent
+
   ],
   imports: [
     BrowserModule
